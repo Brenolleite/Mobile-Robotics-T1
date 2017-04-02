4d4cfd5808cc6a6d381a095fde0300bc6b5311ae
Guilherme Leite <guilherme.vieira.leite@gmail.com>
2017-04-02 02:51:35 -0300

    void collision 1.1

--- Robot.cpp
+++ Robot.cpp
@@ -1,231 +1,255 @@
 #include "Robot.h"
 
 Robot::Robot(Simulator *sim, std::string name) {
-    this->sim = sim;
-    this->name = name;
+  this->sim = sim;
+  this->name = name;
 
-    this->velocity[0] = 10;
-    this->velocity[1] = 10;
+  this->velocity[0] = 10;
+  this->velocity[1] = 10;
 
-    handle = sim->getHandle(name);
+  handle = sim->getHandle(name);
 
-    if (LOG) {
-        FILE *data =  fopen("gt.txt", "wt");
-        if (data!=NULL)
-            fclose(data);
-        data =  fopen("sonar.txt", "wt");
-        if (data!=NULL)
-            fclose(data);
-    }
+  if (LOG) {
+    FILE *data =  fopen("gt.txt", "wt");
+    if (data!=NULL)
+      fclose(data);
+    data =  fopen("sonar.txt", "wt");
+    if (data!=NULL)
+      fclose(data);
+  }
 
-    /* Get handles of sensors and actuators */
-    encoderHandle[0] = sim->getHandle("Pioneer_p3dx_leftWheel");
-    encoderHandle[1] = sim->getHandle("Pioneer_p3dx_rightWheel");
-    std::cout << "Left Encoder: "<< encoderHandle[0] << std::endl;
-    std::cout << "Right Encoder: "<< encoderHandle[1] << std::endl;
+  /* Get handles of sensors and actuators */
+  encoderHandle[0] = sim->getHandle("Pioneer_p3dx_leftWheel");
+  encoderHandle[1] = sim->getHandle("Pioneer_p3dx_rightWheel");
+  std::cout << "Left Encoder: "<< encoderHandle[0] << std::endl;
+  std::cout << "Right Encoder: "<< encoderHandle[1] << std::endl;
 
-    /* Get handles of sensors and actuators */
-    motorHandle[0] = sim->getHandle("Pioneer_p3dx_leftMotor");
-    motorHandle[1] = sim->getHandle("Pioneer_p3dx_rightMotor");
-    std::cout << "Left motor: "<<  motorHandle[0] << std::endl;
-    std::cout << "Right motor: "<<  motorHandle[1] << std::endl;
+  /* Get handles of sensors and actuators */
+  motorHandle[0] = sim->getHandle("Pioneer_p3dx_leftMotor");
+  motorHandle[1] = sim->getHandle("Pioneer_p3dx_rightMotor");
+  std::cout << "Left motor: "<<  motorHandle[0] << std::endl;
+  std::cout << "Right motor: "<<  motorHandle[1] << std::endl;
 
-    simxChar sensorName[31];
-    /* Connect to sonar sensors. Requires a handle per sensor. Sensor name: Pioneer_p3dx_ultrasonicSensorX, where
-     * X is the sensor number, from 1 - 16 */
-    for(int i = 0; i < 16; i++)
+  simxChar sensorName[31];
+  /* Connect to sonar sensors. Requires a handle per sensor. Sensor name: Pioneer_p3dx_ultrasonicSensorX, where
+   * X is the sensor number, from 1 - 16 */
+  for(int i = 0; i < 16; i++)
+  {
+    sprintf(sensorName,"%s%d","Pioneer_p3dx_ultrasonicSensor",i+1);
+    sonarHandle[i] = sim->getHandle(sensorName);
+    if (sonarHandle[i] == -1)
+      std::cout <<  "Error on connecting to sensor " + i+1 << std::endl;
+    else
     {
-        sprintf(sensorName,"%s%d","Pioneer_p3dx_ultrasonicSensor",i+1);
-        sonarHandle[i] = sim->getHandle(sensorName);
-        if (sonarHandle[i] == -1)
-            std::cout <<  "Error on connecting to sensor " + i+1 << std::endl;
-        else
-        {
-            std::cout << "Connected to sensor\n";
-        }
-
-        //init sensors
-        sim->readProximitySensor(sonarHandle[i],NULL,NULL,0);
+      std::cout << "Connected to sensor\n";
     }
 
-    /* Get the robot current absolute position */
-    sim->getObjectPosition(handle,robotPosition);
-    sim->getObjectOrientation(handle,robotOrientation);
+    //init sensors
+    sim->readProximitySensor(sonarHandle[i],NULL,NULL,0);
+  }
 
-    initialPose[0]=robotPosition[0];
-    initialPose[1]=robotPosition[1];
-    initialPose[2]=robotOrientation[2];
-    for(int i = 0; i < 3; i++)
-    {
-        robotLastPosition[i] = robotPosition[i];
-    }
+  /* Get the robot current absolute position */
+  sim->getObjectPosition(handle,robotPosition);
+  sim->getObjectOrientation(handle,robotOrientation);
 
-    /* Get the encoder data */
-    sim->getJointPosition(motorHandle[0],&encoder[0]);
-    sim->getJointPosition(motorHandle[1],&encoder[1]);
-    std::cout << encoder[0] << std::endl;
-    std::cout << encoder[1] << std::endl;
+  initialPose[0]=robotPosition[0];
+  initialPose[1]=robotPosition[1];
+  initialPose[2]=robotOrientation[2];
+  for(int i = 0; i < 3; i++)
+  {
+    robotLastPosition[i] = robotPosition[i];
+  }
+
+  /* Get the encoder data */
+  sim->getJointPosition(motorHandle[0],&encoder[0]);
+  sim->getJointPosition(motorHandle[1],&encoder[1]);
+  std::cout << encoder[0] << std::endl;
+  std::cout << encoder[1] << std::endl;
 
 }
 
 void Robot::update() {
-    drive(this->velocity[0],this->velocity[1]);
-    updateSensors();
-    updatePose();
-    check();
+  drive(this->velocity[0],this->velocity[1]);
+  updateSensors();
+  updatePose();
+  check();
 }
 
 void:: Robot::check()
 {
-    int min = 999;
-    int minIndex = 0;
+  float min = 999.0;
+  int minIndex = 0;
 
-    for (int i=0; i<NUM_SONARS; ++i){
-        if (sonarReadings[i] < min && min > 0){
-            min = sonarReadings[i];
-            minIndex = i;
-        }
+  for (int i = 0; i < 8; ++i)
+  {
+    std::cout << i+1 << "\t" << sonarReadings[i] << std::endl;
+
+    if (sonarReadings[i] < min && sonarReadings[i] > 0.0)
+    {
+      min = sonarReadings[i];
+      minIndex = i;
     }
+  }
 
-    if(min < 1){
-        if(minIndex == 6 || minIndex == 7 || minIndex == 8){
-            this->velocity[0] = 5;
-            this->velocity[1] = 30;
-        }
-        else if(minIndex == 2 || minIndex == 3 || minIndex == 4){
-            this->velocity[0] = 30;
-            this->velocity[1] = 5;
-        }
-    }else{
-        this->velocity[0] = 30;
-        this->velocity[1] = 0;
+  std::cout << "\n";
+
+  if(min < 1.0 && min > 0.0)
+  {
+    if(minIndex == 6 || minIndex == 7 || minIndex == 8)
+    {
+      std::cout << "Turning left\n";
+      // X: Sensor's id number in the simulation
+      // Y: Sensor's reading
+      // i: Sensor id number in the sonar array
+      // X: Y[i]
+      std::cout << "5: " << sonarReadings[6] << std::endl;
+      std::cout << "6: " << sonarReadings[7] << std::endl;
+      std::cout << "7: " << sonarReadings[8] << std::endl;
+
+      this->velocity[0] = 0;
+      this->velocity[1] = 15;
     }
+    else if(minIndex == 1 || minIndex == 2 || minIndex == 3)
+    {
+      std::cout << "Turning right\n";
+      std::cout << "2: " << sonarReadings[1] << std::endl;
+      std::cout << "3: " << sonarReadings[2] << std::endl;
+      std::cout << "4: " << sonarReadings[3] << std::endl;
 
+      this->velocity[0] = 0;
+      this->velocity[1] = -15;
+    }
+  }else{
+    std::cout << "Straight line\n";
+
+    this->velocity[0] = 20;
+    this->velocity[1] = 0;
+  }
 }
 
 void Robot::updateSensors()
 {
 
-    /* Update sonars */
-    for(int i = 0; i < NUM_SONARS; i++)
+  /* Update sonars */
+  for(int i = 0; i < NUM_SONARS; i++)
+  {
+    simxUChar state;       // sensor state
+    simxFloat coord[3];    // detected point coordinates [only z matters]
+
+    /* simx_opmode_streaming -> Non-blocking mode */
+
+    /* read the proximity sensor
+     * detectionState: pointer to the state of detection (0=nothing detected)
+     * detectedPoint: pointer to the coordinates of the detected point (relative to the frame of reference of the sensor) */
+    if (sim->readProximitySensor(sonarHandle[i],&state,coord,1)==1)
     {
-        simxUChar state;       // sensor state
-        simxFloat coord[3];    // detected point coordinates [only z matters]
-
-        /* simx_opmode_streaming -> Non-blocking mode */
-
-        /* read the proximity sensor
-         * detectionState: pointer to the state of detection (0=nothing detected)
-         * detectedPoint: pointer to the coordinates of the detected point (relative to the frame of reference of the sensor) */
-        if (sim->readProximitySensor(sonarHandle[i],&state,coord,1)==1)
-        {
-            if(state > 0)
-                sonarReadings[i] = coord[2];
-            else
-                sonarReadings[i] = -1;
-        }
+      if(state > 0)
+        sonarReadings[i] = coord[2];
+      else
+        sonarReadings[i] = -1;
     }
+  }
 
-    /* Update encoder data */
-    lastEncoder[0] = encoder[0];
-    lastEncoder[1] = encoder[1];
+  /* Update encoder data */
+  lastEncoder[0] = encoder[0];
+  lastEncoder[1] = encoder[1];
 
-    /* Get the encoder data */
-    if (sim->getJointPosition(motorHandle[0], &encoder[0]) == 1);
-        //std::cout << "ok left enconder"<< encoder[0] << std::endl;  // left
-    if (sim->getJointPosition(motorHandle[1], &encoder[1]) == 1);
-        //std::cout << "ok right enconder"<< encoder[1] << std::endl;  // right
+  /* Get the encoder data */
+  if (sim->getJointPosition(motorHandle[0], &encoder[0]) == 1);
+  //std::cout << "ok left enconder"<< encoder[0] << std::endl;  // left
+  if (sim->getJointPosition(motorHandle[1], &encoder[1]) == 1);
+  //std::cout << "ok right enconder"<< encoder[1] << std::endl;  // right
 
 }
 
 void Robot::updatePose()
 {
-    for(int i = 0; i < 3; i++)
-    {
-        robotLastPosition[i] = robotPosition[i];
-    }
+  for(int i = 0; i < 3; i++)
+  {
+    robotLastPosition[i] = robotPosition[i];
+  }
 
-    /* Get the robot current position and orientation */
-    sim->getObjectPosition(handle,robotPosition);
-    sim->getObjectOrientation(handle,robotOrientation);
+  /* Get the robot current position and orientation */
+  sim->getObjectPosition(handle,robotPosition);
+  sim->getObjectOrientation(handle,robotOrientation);
 }
 
 void Robot::writeGT() {
-    /* write data to file */
-    /* file format: robotPosition[x] robotPosition[y] robotPosition[z] robotLastPosition[x] robotLastPosition[y] robotLastPosition[z]
-     *              encoder[0] encoder[1] lastEncoder[0] lastEncoder[1] */
+  /* write data to file */
+  /* file format: robotPosition[x] robotPosition[y] robotPosition[z] robotLastPosition[x] robotLastPosition[y] robotLastPosition[z]
+   *              encoder[0] encoder[1] lastEncoder[0] lastEncoder[1] */
 
-    if (LOG) {
+  if (LOG) {
 
-        FILE *data =  fopen("gt.txt", "at");
-        if (data!=NULL)
-        {
-            for (int i=0; i<3; ++i)
-                fprintf(data, "%.2f\t",robotPosition[i]);
-            for (int i=0; i<3; ++i)
-                fprintf(data, "%.2f\t",robotLastPosition[i]);
-            for (int i=0; i<3; ++i)
-                fprintf(data, "%.2f\t",robotOrientation[i]);
-            for (int i=0; i<2; ++i)
-                fprintf(data, "%.2f\t",encoder[i]);
-            for (int i=0; i<2; ++i)
-                fprintf(data, "%.2f\t",lastEncoder[i]);
-            fprintf(data, "\n");
-            fflush(data);
-            fclose(data);
-          }
-          else
-            std::cout << "Unable to open file";
+    FILE *data =  fopen("gt.txt", "at");
+    if (data!=NULL)
+    {
+      for (int i=0; i<3; ++i)
+        fprintf(data, "%.2f\t",robotPosition[i]);
+      for (int i=0; i<3; ++i)
+        fprintf(data, "%.2f\t",robotLastPosition[i]);
+      for (int i=0; i<3; ++i)
+        fprintf(data, "%.2f\t",robotOrientation[i]);
+      for (int i=0; i<2; ++i)
+        fprintf(data, "%.2f\t",encoder[i]);
+      for (int i=0; i<2; ++i)
+        fprintf(data, "%.2f\t",lastEncoder[i]);
+      fprintf(data, "\n");
+      fflush(data);
+      fclose(data);
     }
+    else
+      std::cout << "Unable to open file";
+  }
 }
 
 void Robot::writeSonars() {
-    /* write data to file */
-    /* file format: robotPosition[x] robotPosition[y] robotPosition[z] robotLastPosition[x] robotLastPosition[y] robotLastPosition[z]
-     *              encoder[0] encoder[1] lastEncoder[0] lastEncoder[1] */
-    if (LOG) {
-        FILE *data =  fopen("sonar.txt", "at");
-        if (data!=NULL)
-        {
-            if (data!=NULL)
-            {
-                for (int i=0; i<NUM_SONARS; ++i)
-                    fprintf(data, "%.2f\t",sonarReadings[i]);
-                fprintf(data, "\n");
-                fflush(data);
-                fclose(data);
-            }
-        }
+  /* write data to file */
+  /* file format: robotPosition[x] robotPosition[y] robotPosition[z] robotLastPosition[x] robotLastPosition[y] robotLastPosition[z]
+   *              encoder[0] encoder[1] lastEncoder[0] lastEncoder[1] */
+  if (LOG) {
+    FILE *data =  fopen("sonar.txt", "at");
+    if (data!=NULL)
+    {
+      if (data!=NULL)
+      {
+        for (int i=0; i<NUM_SONARS; ++i)
+          fprintf(data, "%.2f\t",sonarReadings[i]);
+        fprintf(data, "\n");
+        fflush(data);
+        fclose(data);
+      }
     }
+  }
 }
 
 void Robot::printPose() {
-    std::cout << "[" << robotPosition[0] << ", " << robotPosition[1] << ", " << robotOrientation[2] << "]" << std::endl;
+  std::cout << "[" << robotPosition[0] << ", " << robotPosition[1] << ", " << robotOrientation[2] << "]" << std::endl;
 }
 
 void Robot::move(float vLeft, float vRight) {
-    sim->setJointTargetVelocity(motorHandle[0], vLeft);
-    sim->setJointTargetVelocity(motorHandle[1], vRight);
+  sim->setJointTargetVelocity(motorHandle[0], vLeft);
+  sim->setJointTargetVelocity(motorHandle[1], vRight);
 }
 
 void Robot::stop() {
-    sim->setJointTargetVelocity(motorHandle[0], 0);
-    sim->setJointTargetVelocity(motorHandle[1], 0);
+  sim->setJointTargetVelocity(motorHandle[0], 0);
+  sim->setJointTargetVelocity(motorHandle[1], 0);
 }
 
 void Robot::drive(double vLinear, double vAngular)
 {
-    sim->setJointTargetVelocity(motorHandle[0], vLToDrive(vLinear,vAngular));
-    sim->setJointTargetVelocity(motorHandle[1], vRToDrive(vLinear,vAngular));
+  sim->setJointTargetVelocity(motorHandle[0], vLToDrive(vLinear,vAngular));
+  sim->setJointTargetVelocity(motorHandle[1], vRToDrive(vLinear,vAngular));
 }
 
 double Robot::vRToDrive(double vLinear, double vAngular)
 {
-    return (((2*vLinear)+(L*vAngular))/2*R);
+  return (((2*vLinear)+(L*vAngular))/2*R);
 }
 
 double Robot::vLToDrive(double vLinear, double vAngular)
 {
-    return (((2*vLinear)-(L*vAngular))/2*R);
+  return (((2*vLinear)-(L*vAngular))/2*R);
 
 }
